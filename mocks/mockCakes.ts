e40d936dec727cec862498549c8142d1774ccf02
Michelle Campbell <m.campbell.tkd@gmail.com>
2026-06-26 13:44:42 +0100

    cake types and mock data

---
+++ mocks/mockCakes.ts
@@ -0,0 +1,28 @@
+import { Cake } from '@/types/cake';
+
+export const mockCakes: Cake[] = [
+  {
+    id: 1,
+    name: 'Chocolate Fudge Cake',
+    comment: 'Rich, dense and absolutely delicious.',
+    imageUrl:
+      'https://images.unsplash.com/photo-1578985545062-69928b1d9587?w=400',
+    yumFactor: 5,
+  },
+  {
+    id: 2,
+    name: 'Lemon Drizzle',
+    comment: 'Light, zesty and perfectly moist.',
+    imageUrl:
+      'https://images.unsplash.com/photo-1464349095431-e9a21285b5f3?w=400',
+    yumFactor: 4,
+  },
+  {
+    id: 3,
+    name: 'Victoria Sponge',
+    comment: 'A classic British favourite.',
+    imageUrl:
+      'https://images.unsplash.com/photo-1486427944299-d1955d23e34d?w=400',
+    yumFactor: 4,
+  },
+];
